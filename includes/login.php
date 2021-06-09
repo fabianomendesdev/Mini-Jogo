38205d363865a1ac16ef7a8a5db2d7dedcd26f22
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-06-09 14:36:58 -0300

    Finalização da parte funcional da pagina registrar

--- includes/login.php
+++ includes/login.php
@@ -4,7 +4,7 @@
 	<link rel="stylesheet" href="assets/css/style.css">
 	
 	<div class="container-login">
-		<form class="form-login" action="index.php?i=validacao" method="POST">
+		<form class="form-login" action="index.php?i=selecao" method="POST">
 			<p id="txtAviso">Login / <a href="index.php?i=cadastro"> cadastre-se</a></p>
 			
 			<input type="email" placeholder="Digite seu e-mail" name="email" id="email" class="inputs">
--- includes/validacao.php
+++ includes/validacao.php
@@ -4,16 +4,20 @@
     
     $createTable = "create table if not exists pessoa( id int auto_increment, email varchar(256) not null unique, senha varchar(25) not null, primary key(id) )default charset = 'utf8'";
 	$mysqli->query($createTable) or die ($mysqli->error);
-    $consulta = "insert into pessoa values (default, '{$email}', '{$senha}')";
-	$con = $mysqli->query($consulta) or die ($mysqli->error);
+    $consulta = "select email from pessoa where email = '$email'";
+    $con = $mysqli->query($consulta) or die ($mysqli->error); 
+
+    if(mysqli_num_rows($con) > 0){
+        echo "<p>Esse email já está cadastrado</p>";
+    }else{
+        $consulta = "insert into pessoa values (default, '$email', '$senha')";
+	    $con = $mysqli->query($consulta) or die ($mysqli->error);
+        echo "<p>Cadastrado com sucesso</p>";
+    }
 ?>
 <main>
     <div class="container-validacao">
-        <?php
-            if(!$mysqli->error){
-                echo "<p>Cadastrado com sucesso</p>";
-            }
-        ?>
+        <a href="index.php?i=login">Login</a>
         <h1>Validação</h1>
     </div>    
 </main>
