e4e6f075174a39b2c2b46eb7d80f93814fc960ba
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-06-09 14:52:27 -0300

    Limitação do tamanho da senha

--- includes/cadastro.php
+++ includes/cadastro.php
@@ -9,9 +9,9 @@
 			
 			<input type="email" placeholder="Digite seu e-mail" name="email" id="email" class="inputs">
 			
-			<input type="text" placeholder="Digite sua senha" name="senha" id="senha" class="inputs">
+			<input type="text" placeholder="Digite sua senha" name="senha" id="senha" class="inputs" max='25'>
 
-			<input type="text" placeholder="Repita sua senha" name="senhaRepetição" id="senhaRepeticao" class="inputs">
+			<input type="text" placeholder="Repita sua senha" name="senhaRepetição" id="senhaRepeticao" class="inputs" max='25'>
 			
 			<input type="submit" value="Cadastrar" id="submit">	
 		</form>
