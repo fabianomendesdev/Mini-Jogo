b91110b84b1f54b2ac194ee4fe2acbc56204f392
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-07-14 14:32:08 -0300

    Update verificationLoggedOut.php

--- includes/verificationLoggedOut.php
+++ includes/verificationLoggedOut.php
@@ -4,6 +4,9 @@
     }
 
     if(!isset($_SESSION['user'])){
+        $exp = time() + (60 * 60 * 2);
+        setcookie('user', $_SESSION['user'], $exp);
+        session_regenerate_id();
         header('Location: login.php');
     }
 ?>
